5a1412b37c755c1d7823f8cebad1095b3992799d
Patrick Coutinho <patrick@istyle.com.br>
2023-06-16 17:15:22 -0300

    feat: added mutation to create item

--- src/graphql/Link.ts
+++ src/graphql/Link.ts
@@ -1,4 +1,4 @@
-import { extendType, objectType } from "nexus";
+import { extendType, nonNull, objectType, stringArg } from "nexus";
 import { NexusGenObjects } from "../../nexus-typegen";
 
 export const Link = objectType({
@@ -10,7 +10,7 @@ export const Link = objectType({
   },
 });
 
-let links: NexusGenObjects["Link"][] = [
+const links: NexusGenObjects["Link"][] = [
   {
     id: 1,
     url: "www.howtographql.com",
@@ -30,6 +30,35 @@ export const LinkQuery = extendType({
       type: "Link",
       resolve(parent, args, context, info) {
         return links;
+      },
+    });
+  },
+});
+
+export const LinkMutation = extendType({
+  type: "Mutation",
+  definition(t) {
+    t.nonNull.field("post", {
+      type: "Link",
+      args: {
+        description: nonNull(stringArg()),
+        url: nonNull(stringArg()),
+      },
+
+      resolve(parent, args, context) {
+        const { description, url } = args;
+
+        const id = links.length + 1;
+
+        const link = {
+          id,
+          description,
+          url,
+        };
+
+        links.push(link);
+
+        return link;
       },
     });
   },
