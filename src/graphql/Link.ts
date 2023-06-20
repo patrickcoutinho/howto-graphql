4de3a317a76e76728651da01c9c5cd0469790dbe
Patrick Coutinho <patrick@istyle.com.br>
2023-06-20 16:54:30 -0300

    feat: added auth

--- src/graphql/Link.ts
+++ src/graphql/Link.ts
@@ -1,5 +1,15 @@
 import { extendType, intArg, nonNull, objectType, stringArg } from "nexus";
-import { NexusGenObjects } from "../../nexus-typegen";
+import { Context } from "../context";
+
+const checkAuth = (context: Context): number => {
+  const { userId } = context;
+
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
+
+  return userId;
+};
 
 export const Link = objectType({
   name: "Link",
@@ -7,6 +17,14 @@ export const Link = objectType({
     t.nonNull.int("id");
     t.nonNull.string("description");
     t.nonNull.string("url");
+    t.field("postedBy", {
+      type: "User",
+      resolve(parent, args, context) {
+        return context.prisma.link
+          .findUnique({ where: { id: parent.id } })
+          .postedBy();
+      },
+    });
   },
 });
 
@@ -16,6 +34,8 @@ export const LinkQuery = extendType({
     t.nonNull.list.nonNull.field("feed", {
       type: "Link",
       resolve(parent, args, context, info) {
+        checkAuth(context);
+
         return context.prisma.link.findMany();
       },
     });
@@ -31,6 +51,8 @@ export const LinkQuery = extendType({
         ),
       },
       resolve(parent, args, context) {
+        checkAuth(context);
+
         const link = context.prisma.link.findUnique({
           where: {
             id: args.linkId,
@@ -54,16 +76,19 @@ export const LinkMutation = extendType({
       },
 
       resolve(parent, args, context) {
+        const userId = checkAuth(context);
+
         const { description, url } = args;
 
-        const link = context.prisma.link.create({
+        const newLink = context.prisma.link.create({
           data: {
             description,
             url,
+            postedBy: { connect: { id: userId } },
           },
         });
 
-        return link;
+        return newLink;
       },
     });
 
@@ -76,6 +101,8 @@ export const LinkMutation = extendType({
       },
 
       resolve(parent, args, context) {
+        checkAuth(context);
+
         const { id, description, url } = args;
 
         const link = context.prisma.link.update({
@@ -99,6 +126,8 @@ export const LinkMutation = extendType({
       },
 
       resolve(parent, args, context) {
+        checkAuth(context);
+
         const { id } = args;
 
         const deletedLink = context.prisma.link.delete({
